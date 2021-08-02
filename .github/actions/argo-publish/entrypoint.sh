5288b3819baaa20624d8b4119f27b6b22132d5b5
Erin Horning <ehorning@virtru.com>
2021-08-02 17:50:40 -0400

    buildimage

--- .github/actions/argo-publish/entrypoint.sh
+++ .github/actions/argo-publish/entrypoint.sh
@@ -8,6 +8,6 @@
 
 aws eks update-kubeconfig --region us-west-2 --name k8s-mgmt
 
-argo submit -n argo-events ./.argo/workflow.yaml -p gitRepoName=virtru-charts -p chartDirectory=cse -p buildImage=833190184321.dkr.ecr.us-west-2.amazonaws.com/virtru-builder:1.0.16 --wait --log
+argo submit -n argo-events ./.argo/workflow.yaml -p gitRepoName=virtru-charts -p chartDirectory=cse --wait --log
 
-argo submit -n argo-events ./.argo/workflow.yaml -p gitRepoName=virtru-charts -p chartDirectory=cks -p buildImage=833190184321.dkr.ecr.us-west-2.amazonaws.com/virtru-builder:1.0.16 --wait --log
+argo submit -n argo-events ./.argo/workflow.yaml -p gitRepoName=virtru-charts -p chartDirectory=cks --wait --log
