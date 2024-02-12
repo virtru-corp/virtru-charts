55fed5415f139ca3923e7750d75488dda9388213
Elizabeth Healy <ehealy@virtru.com>
2024-02-12 11:37:11 -0500

    add external base url override to script

--- audit-api/example/deploy-with-istio.sh
+++ audit-api/example/deploy-with-istio.sh
@@ -2,6 +2,8 @@
 imagePullUsername="changeme"
 # Image pull username
 imagePullPAT="changeme"
+# Hostname for ingress
+ingressHostname=""
 # Scale istiod downt to 0 then back up to 1
 scaleIstio=false
 
@@ -10,7 +12,7 @@ auditChart="${chartRepo}/audit-api"
 # For local install change to chart-version.tgz
 # auditChart="../../audit-api-0.1.0.tgz"
 # Is the platform in the same cluster as the audit-api deployment
-while getopts "u:p:o:d:i" arg; do
+while getopts "u:p:o:d:h:i" arg; do
   case $arg in
     u)
       imagePullUsername=${OPTARG}
@@ -24,6 +26,9 @@ while getopts "u:p:o:d:i" arg; do
     d)
       chartsLocalDir=${OPTARG}
       ;;
+    h)
+      ingressHostname=${OPTARG}
+      ;;
     i)
       scaleIstio=true
       ;;
@@ -36,7 +41,9 @@ if [ ! -z "$chartsLocalDir" ]; then
 fi
 
 
-echo "Deploying with chart overrides = ${overrideValues}"
+echo "Deploying to hostname=${ingressHostname} with chart overrides = ${overrideValues}"
+
+externalBaseUrl="https://${ingressHostname}"
 
 echo "#1 Deploy audit-api Service"
 helm upgrade --install -n $ns --create-namespace \
@@ -45,6 +52,7 @@ helm upgrade --install -n $ns --create-namespace \
   --set "secrets.imageCredentials.pull-secret.password"="${imagePullPAT}" \
   --set "secrets.imageCredentials.pull-secret.email"="nope@nah.com" \
   --set "secrets.imageCredentials.pull-secret.registry"="ghcr.io" \
+  --set "istioAuth.oidc.oidcExternalBaseUrl"="${externalBaseUrl}" \
   -f $overrideValues \
   audit-api $auditChart
 
